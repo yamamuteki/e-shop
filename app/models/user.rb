0307f45d74d6e6d73f7ccccbb6e2b1e26c53fe19
yama@muteki <yama@muteki.com>
2017-06-20 21:07:23 +0900

    Add auto user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable, :omniauthable
 
   def self.from_omniauth(auth)
-    where(email: auth.info.email).first
+    email = auth.info.email
+    user = where(email: email).first
+    unless user
+      user = User.create!(email: email, password: SecureRandom.base64(128)[0..127])
+      user.confirm
+    end
+    user
   end
 end
